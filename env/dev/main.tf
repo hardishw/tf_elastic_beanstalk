763c718720182707a8118968cf2cae5fd648e647
hardishw <hardish_95@hotmail.co.uk>
2019-12-10 18:17:36 +0000

    correct syntax

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -7,61 +7,37 @@ terraform {
 module "dev_elastic_beanstalk" {
   source = "../"
 
-  variable "region" {
-    description = "region to deploy infrastructure to"
-    type        = "string"
-    default     = "eu-west-2"
+  region = "eu-west-2"
+
+  bucket_name = "hardishwilkhu"
+
+  application = {
+    path                = "sample.war"
+    name                = "tomcat-test"
+    description         = "test application"
+    version             = "1"
+    version_description = "this is an example"
+    nexus_url           = ""
   }
 
-  variable "bucket_name" {
-    description = "bucket to deploy artifacts to"
-    type        = "string"
-    default     = "hardishwilkhu"
+  environment = {
+    name        = "tomcat-env"
+    stack       = "64bit Amazon Linux 2018.03 v3.3.0 running Tomcat 8.5 Java 8"
+    vpc_id      = "vpc-8b05abee"
+    ssl_cert_id = ""
+    #eg "subnet-11111111,subnet-22222222"
+    instance_subnets = ""
+    elb_subnets      = ""
   }
 
-  variable "application" {
-    description = "beanstalk application details"
-    type        = "map"
-    default = {
-      path                = "sample.war"
-      name                = "tomcat-test"
-      description         = "test application"
-      version             = "1"
-      version_description = "this is an example"
-      nexus_url           = ""
-    }
+  env_vars = {
+    name = "dev"
   }
 
-  variable "environment" {
-    description = "beanstalk environment configuration"
-    type        = "map"
-    default = {
-      name        = "tomcat-env"
-      stack       = "64bit Amazon Linux 2018.03 v3.3.0 running Tomcat 8.5 Java 8"
-      vpc_id      = "vpc-8b05abee"
-      ssl_cert_id = ""
-      #eg "subnet-11111111,subnet-22222222"
-      instance_subnets = ""
-      elb_subnets      = ""
-    }
-  }
-
-  variable "env_vars" {
-    description = "beanstalk environment variables"
-    type        = "map"
-    default = {
-      name = "dev"
-    }
-  }
-
-  variable "tags" {
-    description = "infrastructure tags"
-    type        = "map"
-    default = {
-      Environment = "dev"
-      Team        = "devops"
-      Owner       = "hardish"
-    }
+  tags = {
+    Environment = "dev"
+    Team        = "devops"
+    Owner       = "hardish"
   }
 
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "region" {
   description = "region to deploy infrastructure to"
-  type        = "string"
+  type        = string
   default     = "eu-west-2"
 }
 
 variable "bucket_name" {
   description = "bucket to deploy artifacts to"
-  type        = "string"
+  type        = string
   default     = "hardishwilkhu"
 }
 
 variable "application" {
   description = "beanstalk application details"
-  type        = "map"
+  type        = map
   default = {
     path                = "sample.war"
     name                = "tomcat-test"
@@ -25,7 +25,7 @@ variable "application" {
 
 variable "environment" {
   description = "beanstalk environment configuration"
-  type        = "map"
+  type        = map
   default = {
     name        = "tomcat-env"
     stack       = "64bit Amazon Linux 2018.03 v3.3.0 running Tomcat 8.5 Java 8"
@@ -39,7 +39,7 @@ variable "environment" {
 
 variable "env_vars" {
   description = "beanstalk environment variables"
-  type        = "map"
+  type        = map
   default = {
     name = "dev"
   }
@@ -47,7 +47,7 @@ variable "env_vars" {
 
 variable "tags" {
   description = "infrastructure tags"
-  type        = "map"
+  type        = map
   default = {
     Environment = "dev"
     Team        = "devops"
